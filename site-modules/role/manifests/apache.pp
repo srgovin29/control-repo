fee3866eb1528332227a089e3f1c5c120047b10c
Srinivasan <srgovin29@outlook.com>
2024-01-18 23:05:24 +0800

    Update apache.pp

--- site-modules/role/manifests/apache.pp
+++ site-modules/role/manifests/apache.pp
@@ -2,7 +2,7 @@
 # install apache webserver with a single webhost
 
 class role::apache (
-  String[1] $webhost		    = $::hostname,
+  String[1] $webhost		    = "puppetagent02.devops.com",
   Stdlib::Port $port		    = 80,
   Stdlib::Unixpath $docroot	= "/var/www/${webhost}",
 ) {
