aee3f0976001265dd6747fc23c146db9737e7681
Srinivasan <srgovin29@outlook.com>
2024-01-18 23:42:44 +0800

    Update apache.pp

--- site-modules/role/manifests/apache.pp
+++ site-modules/role/manifests/apache.pp
@@ -1,5 +1,5 @@
 class role::apache (
-  String[1] $webhost        = $::hostname,
+  String[1] $webhost        = "puppetagent02",
   Stdlib::Port $port        = 80,
   Stdlib::Unixpath $docroot  = "/var/www/${webhost}",
 ) {
