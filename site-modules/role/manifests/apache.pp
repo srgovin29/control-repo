e8c3972cf1fac6a3dcf2c456d77e8bf000e8f701
Srinivasan <srgovin29@outlook.com>
2024-01-18 23:36:38 +0800

    Update apache.pp

--- site-modules/role/manifests/apache.pp
+++ site-modules/role/manifests/apache.pp
@@ -1,10 +1,7 @@
-# role:apache
-# install apache webserver with a single webhost
-
 class role::apache (
-  String[1] $webhost		    = $::hostname,
-  Stdlib::Port $port		    = 80,
-  Stdlib::Unixpath $docroot	= "/var/www/${webhost}",
+  String[1] $webhost        = $::hostname,
+  Stdlib::Port $port        = 80,
+  Stdlib::Unixpath $docroot  = "/var/www/${webhost}",
 ) {
   class { 'profile::apache':
     webhost  => $webhost,
