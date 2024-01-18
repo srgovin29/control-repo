d87885119a6864c5e0f182f824d5202426add994
Srinivasan <srgovin29@outlook.com>
2024-01-18 18:56:39 +0800

    testing apache

---
+++ site-modules/role/manifests/apache.pp
@@ -0,0 +1,14 @@
+# role:apache
+# install apache webserver with a single webhost
+
+class role::apache (
+  String[1] $webhost		= $::hostname,
+  Stdlib::Port $port		= 80,
+  Stdlib::Unixpath $docroot	= "/var/www/${webhost}",
+) {
+  class { 'profile::apache':
+    webhost  => $webhost,
+    port     => $port,
+    docroot  => $docroot,
+  }
+}
