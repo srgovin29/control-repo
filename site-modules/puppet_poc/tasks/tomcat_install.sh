d967919367e3e6a9543182559bcb581e188476a9
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-20 13:08:00 +0800

    Added tomcat pacakge installtion steps

---
+++ site-modules/puppet_poc/tasks/tomcat_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+apphome = $PT_apphome
+appport    = $PT_appport
+if [ ! -d "$apphome" ]; then
+  echo "$apphome does not exist."
+  mkdir -p /opt/tomcat 
+  chown tomcat:tomcat -R /opt/tomcat
+fi
+if [ !  -f /opt/tomcat/apache-tomcat-8.5.98.tar.gz ]; then 
+      echo "/opt/tomcat/apache-tomcat-8.5.98.tar.gz file not downloaded" 
+      /usr/bin/wget -o /opt/tomcat/apache-tomcat-8.5.98.tar.gz https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.98/bin/apache-tomcat-8.5.98.tar.gz
+      /usr/bin/tar -xzf /opt/tomcat/apache-tomcat-8.5.98.tar.gz -C /opt/tomcat/
+      /usr/bin/ln -s /opt/tomcat/apache-tomcat-8.5.37 /opt/tomcat/latest
+      chown -R tomcat: /opt/tomcat
+      chmod +x /opt/tomcat/latest/bin/*.sh
+fi
+echo "Adding Firewall to port 8080"
+firewall-cmd --zone=public --permanent --add-port="$appport/tcp"
+firewall-cmd --reload
