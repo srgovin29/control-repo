ea37bd588d91dad7be70db97ac8d696522fca47c
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-20 18:20:34 +0800

    mysql-server plan installed

--- site-modules/puppet_poc/plans/mysql.pp
+++ site-modules/puppet_poc/plans/mysql.pp
@@ -1,7 +1,6 @@
 ## This plan is to install mysql server
 plan puppet_poc::mysql(
   TargetSpec   $webnodes,
-  Integer      $dbnodes,
 ) {
   $mysql_results = apply($dbnodes, '_catch_errors' => true ) {
     package { 'mysql-server':
