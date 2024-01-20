7d7aec17af4d2df881216c0bbbed720b1080f405
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-20 13:52:45 +0800

    tomcat issue

--- site-modules/puppet_poc/tasks/tomcat_install.sh
+++ site-modules/puppet_poc/tasks/tomcat_install.sh
@@ -10,7 +10,13 @@ if [ !  -f /opt/tomcat/apache-tomcat-8.5.98.tar.gz ]; then
       echo "/opt/tomcat/apache-tomcat-8.5.98.tar.gz file not downloaded" 
       /usr/bin/wget -o /opt/tomcat/apache-tomcat-8.5.98.tar.gz https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.98/bin/apache-tomcat-8.5.98.tar.gz
       /usr/bin/tar -xzf /opt/tomcat/apache-tomcat-8.5.98.tar.gz -C /opt/tomcat/
-      /usr/bin/ln -s /opt/tomcat/apache-tomcat-8.5.37 /opt/tomcat/latest
+      /usr/bin/ln -s /opt/tomcat/apache-tomcat-8.5.98 /opt/tomcat/latest
+      chown -R tomcat: /opt/tomcat
+      chmod +x /opt/tomcat/latest/bin/*.sh
+else
+      echo "/opt/tomcat/apache-tomcat-8.5.98.tar.gz file already there, extracing bin "
+      /usr/bin/tar -xzf /opt/tomcat/apache-tomcat-8.5.98.tar.gz -C /opt/tomcat/
+      /usr/bin/ln -s /opt/tomcat/apache-tomcat-8.5.98 /opt/tomcat/latest
       chown -R tomcat: /opt/tomcat
       chmod +x /opt/tomcat/latest/bin/*.sh
 fi
