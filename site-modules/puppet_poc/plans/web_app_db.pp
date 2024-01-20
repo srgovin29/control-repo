3abbfcbea5ed3d7834f3e61f36d17ddf1f1a5dce
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-20 23:07:31 +0800

    added 3 tier service status

--- site-modules/puppet_poc/plans/web_app_db.pp
+++ site-modules/puppet_poc/plans/web_app_db.pp
@@ -1,9 +1,8 @@
 ## This plan is to install mysql server
-plan puppet_poc::mysql(
+plan puppet_poc::web_app_db(
   TargetSpec   $dbnodes = 'puppetagent03.devops.com',
   TargetSpec   $webnodes = 'puppetagent01.devops.com',
   TargetSpec   $appnodes = 'puppetagent02.devops.com',
-
   #app configuration parameter
   String       $appuser  = 'tomcat',
   String       $appgrp   = 'tomcat',
@@ -12,14 +11,12 @@ plan puppet_poc::mysql(
   String       $apphome  = '/opt/tomcat',
   Integer      $appport  = 8080,
   #web configuration parameter
-
   String $webuser = 'apacheadm',
   String $webgrp  = 'apachegrp',
   Integer $webuid =  14501,
   Integer $webgid = 14501,
   String $webpkg = 'httpd',
   String $websvc = 'httpd'
-
 ) {
   $web_status = run_command( "service status ${websvc}", $webnodes )
   out::message("result is : ${web_status}")
