379a24854c49e4eda9d9836526d2cadf706afa0e
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-21 10:32:12 +0800

    added 3 tier service status

--- site-modules/puppet_poc/plans/web_app_db.pp
+++ site-modules/puppet_poc/plans/web_app_db.pp
@@ -21,34 +21,6 @@ plan puppet_poc::web_app_db(
   # DB configuration parameter
   String $dbsvc = 'mysqld',
 ) {
-  ### Starting web service 
-  $web_status = run_command( "systemctl show -p SubState -p ActiveState ${websvc}", $webnodes )
-  notice("value is: ${web_status}")
-  $web_status.to_data.each | $result | {
-    notice("result is :${result}")
-    $web_status_res = $result['value']['stdout']
-    notice("value for stdout: ${web_status_res}")
-    # $web_status.to_data.each | $result_hash | {
-    # $web_status_res = $result_hash['result']['stdout']
-    if $web_status_res != "ActiveState=active\nSubState=running\n" {
-    fail_plan("Webservice named ${websvc} is not running , so plan fail here itself. 
-       Can you please login ${webnodes} and verify the status" )
-    }   else {
-    out::message("Web Service is up and running and the status is : ${web_status_res}") }
-  }
-  #### Starting App service 
-  $app_status = run_command( "systemctl show -p SubState -p ActiveState ${appsvc}", $appnodes )
-  $app_status.to_data.each | $app_result | {
-    notice("result is :${app_result}")
-    $app_status_res = $app_result['value']['stdout']
-    notice("value for stdout: ${app_status_res}")
-    if $app_status_res != "ActiveState=active\nSubState=running\n" {
-    fail_plan("App Service named ${appsvc} is not running , so plan fail here itself. 
-       Can you please login ${appnodes} and verify the status" )
-    }   else {
-      out::message("App Service is up and running and the status is : ${app_status_res}")
-    }
-  }
   ### Starting DB service 
   $db_status = run_command( "systemctl show -p SubState -p ActiveState ${dbsvc}", $dbnodes )
   $db_status.to_data.each | $db_result | {
@@ -61,5 +33,33 @@ plan puppet_poc::web_app_db(
     }   else {
       out::message("DB Service is up and running and the status is : ${db_status_res}")
     }
+  }
+  #### Starting App service 
+  $app_status = run_command( "systemctl show -p SubState -p ActiveState ${appsvc}", $appnodes )
+  $app_status.to_data.each | $app_result | {
+    notice("result is :${app_result}")
+    $app_status_res = $app_result['value']['stdout']
+    notice("value for stdout: ${app_status_res}")
+    if $app_status_res != "ActiveState=active\nSubState=running\n" {
+    fail_plan("DB service is up, But App Service named ${appsvc} is not running , so plan fail here itself. 
+       Can you please login ${appnodes} and verify the status" )
+    }   else {
+      out::message("DB and App Services are up and running. App service status is : ${app_status_res}")
+    }
+  }
+  ### Starting web service 
+  $web_status = run_command( "systemctl show -p SubState -p ActiveState ${websvc}", $webnodes )
+  notice("value is: ${web_status}")
+  $web_status.to_data.each | $result | {
+    notice("result is :${result}")
+    $web_status_res = $result['value']['stdout']
+    notice("value for stdout: ${web_status_res}")
+    # $web_status.to_data.each | $result_hash | {
+    # $web_status_res = $result_hash['result']['stdout']
+    if $web_status_res != "ActiveState=active\nSubState=running\n" {
+    fail_plan("DB and App services are running but Webservice named ${websvc} is not running , so plan fail here itself. 
+       Can you please login ${webnodes} and verify the status" )
+    }   else {
+    out::message("DB,App and Web Service are up and running. Web service status is : ${web_status_res}") }
   }
 }
