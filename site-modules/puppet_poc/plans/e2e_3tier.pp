73db8c590e0567f40a4f1accfc87393543b13b87
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-21 14:13:26 +0800

    added 3 tier service status - e2e solution

--- site-modules/puppet_poc/plans/e2e_3tier.pp
+++ site-modules/puppet_poc/plans/e2e_3tier.pp
@@ -34,7 +34,7 @@ plan puppet_poc::e2e_3tier(
     '_catch_errors' => true,
   )
   $web_e2e_result.to_data.each | $web_results | {
-    $web_output = { $web_results.target => {
+    $web_output = { $web_results['target'] => {
         'status' => $web_results['status'],
         'log' => $web_results['value']['report']['logs'],
         'output' => $web_results['value']['_output'],
