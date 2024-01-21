dcff00364ba19b1af10d07f5f70accf01c693789
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-21 22:26:36 +0800

    3 tier arch e2e testing - tomcat

--- site-modules/puppet_poc/plans/e2e_3tier.pp
+++ site-modules/puppet_poc/plans/e2e_3tier.pp
@@ -10,7 +10,7 @@ plan puppet_poc::e2e_3tier(
   Integer      $appgid   = 15001,
   String       $apphome  = '/opt/tomcat',
   Integer      $appport  = 8080,
-  String       $appsvc   = tomcat,
+  String       $appsvc   = 'tomcat',
   #web configuration parameter
   String $webuser = 'apacheadm',
   String $webgrp  = 'apachegrp',
