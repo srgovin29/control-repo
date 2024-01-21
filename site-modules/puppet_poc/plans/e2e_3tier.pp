6922bc2e45bb8d9b564cc90ba6c8620a5d488079
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-21 13:57:15 +0800

    added 3 tier service status - e2e solution

--- site-modules/puppet_poc/plans/e2e_3tier.pp
+++ site-modules/puppet_poc/plans/e2e_3tier.pp
@@ -1,9 +1,4 @@
 ## This plan is E2E setup for 3 tier Application 
-$final_result = {}
-$final_result['web'] = {}
-$final_result['app'] = {}
-$final_result['db'] = {}
-
 plan puppet_poc::e2e_3tier(
   TargetSpec   $dbnodes = 'puppetagent03.devops.com',
   TargetSpec   $webnodes = 'puppetagent01.devops.com',
@@ -26,6 +21,7 @@ plan puppet_poc::e2e_3tier(
   # DB configuration parameter
   String $dbsvc = 'mysqld',
 ) {
+  $final_result = {}
   #### Setup Web Application 
   $web_e2e_result = run_plan( 'puppet_poc::apache_e2e',
     webnodes => $webnodes,
@@ -38,10 +34,13 @@ plan puppet_poc::e2e_3tier(
     '_catch_errors' => true,
   )
   $web_e2e_result.each | $web_results | {
-    final_result['web']['target'] = $web_results.target
-    final_result['web']['target']['status'] =   $web_results.status
-    final_result['web']['target']['log'] = $web_results.value.report.logs
-    final_result['web']['target']['output'] = $web_results.value['_output']
+    $web_output = { $web_results.target => {
+        'status' => $web_results.status,
+        'log' => $web_results.value.report.logs,
+        'output' => $web_results.value['_output'],
+      },
+    }
+    $final_result = { 'web_output' => $web_output }
   }
   out::message("Results from web server e2e : ${final_result}")
 }
