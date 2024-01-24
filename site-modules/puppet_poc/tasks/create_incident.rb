3658ea34b5be2235618d48cfd0a72da761c69efc
karthikeyan kanagaraj <karthikeyankanagaraj@karthikeyans-MacBook-Air.local>
2024-01-24 18:26:55 +0800

    Creating ticekt SNOW ticket using task

--- site-modules/puppet_poc/tasks/create_incident.rb
+++ site-modules/puppet_poc/tasks/create_incident.rb
@@ -26,6 +26,11 @@ request["Authorization"] = $token
 
 # url = URI("https://dev218806.service-now.com/api/now/table/incident?sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_fields=number,short_description,sys_created_on,state,sys_created_by,impact,priority,caller_id,description")
 # request["Cookie"] = "BIGipServerpool_dev218806=276864778.58176.0000; JSESSIONID=61BB83B8FC0BD6674B1EA111D27C7F62; glide_session_store=9886FBE347F331101792D879316D4362; glide_user_route=glide.35672988f823e85c84a3b19747181b6e"
+begin
+    response = https.request(request)
+    puts response.read_body
+rescue Puppet::Error => e
+    puts({ status: 'failure', error: e.message }.to_json)
+    exit 1    
+end
 
-response = https.request(request)
-puts response.read_body
